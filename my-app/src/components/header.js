c6f68f793fe385dc4e391c37d62162359ca7be7a
Gio <gioandrewlucero@gmail.com>
2020-11-12 13:51:05 -0500

    Added animations.

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
-import logo from '../images/logo-designcode.svg';
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
   <div className="HeaderGroup">
-    <Link to="/"><img width="30" /><img src={logo} width="30" /></Link>
+    <Link to="/"><img width="30" src={require('../images/logo-designcode.svg')} /></Link>
     <Link to="/courses">Courses</Link>
     <Link to="/downloads">Downloads</Link>
     <Link to="/workshops">Workshops</Link>
